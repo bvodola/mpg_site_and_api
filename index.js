38c16498732f9ee7e229364196d8f1b9d6b2bb37
Brunno Vodola <bvodola@gmail.com>
2016-10-06 12:05:26 -0300

    Adding SASS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Imports
+var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
+var sass = require('node-sass-middleware');
 
 // ===============
 // App Definitions
@@ -11,6 +13,15 @@ var port = (process.env.HOSTNAME == 'web506.webfaction.com' ? 99999 : 4000);
 // ==========
 // Middleware
 // ==========
+
+app.use(sass({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+		indentedSyntax: true,
+    prefix:  '/'
+}));
 app.use(express.static('public'));
 
 //========
