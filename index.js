61702679166f99e82073ce9f2b203e321cf29386
Brunno Vodola <bvodola@gmail.com>
2016-10-17 15:57:29 -0200

    First Commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var sass = require('node-sass-middleware');
 
 // ===============
@@ -10,10 +12,36 @@ var sass = require('node-sass-middleware');
 var app = express();
 var port = (process.env.HOSTNAME == 'web506.webfaction.com' ? 99999 : 4000);
 
+var Schema = mongoose.Schema;
+mongoose.connect('mongodb://bvodola:qZwX1001@ds033086.mlab.com:33086/landing_db');
+
+
+// =======
+// Schemas
+// =======
+
+var salesSchema = new Schema({
+    sale_id: String,
+    name: String,
+    type: String,
+    amount: String,
+    email: String,
+    phone: String,
+    client: String,
+    created: { type: Date, default: Date.now }
+  },
+  {
+    strict: false
+  }
+);
+
+var Sale = mongoose.model('sales', salesSchema);
+
 // ==========
 // Middleware
 // ==========
 
+// SASS Support
 app.use(sass({
     src: path.join(__dirname, 'sass'),
     dest: path.join(__dirname, 'public'),
@@ -22,13 +50,34 @@ app.use(sass({
 		indentedSyntax: true,
     prefix:  '/'
 }));
-app.use(express.static('public'));
+
+app.use(express.static('public')); // Public Folder
+app.use(bodyParser.json()); // Support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Support encoded bodies
 
 //========
 // Routing
 //========
 app.get('/', function(req, res) {
 	res.sendFile(__dirname+'/public/index.html');
+});
+
+app.post('/sale', function(req, res) {
+  var sale_id = (typeof req.body.order_id !== 'undefined') ? req.body.order_id : '';
+  var amount = (typeof req.body.amount !== 'undefined') ? req.body.amount : '';
+  var client = (typeof req.body.client !== 'undefined') ? req.body.client : '';
+
+  Sale.create({
+    sale_id: sale_id,
+    amount: amount,
+    client: client
+  }, function (err, sale) {
+    if (err) {
+      console.log(err);
+    }
+    // Document saved.
+    res.sendStatus(200);
+  })
 });
 
 // ===============
