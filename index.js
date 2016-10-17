7cdcbb1dae8d0772619483b06bdf19daa8ce788c
Brunno Vodola <bvodola@gmail.com>
2016-10-17 16:33:09 -0200

    Commited some stuff

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ app.use(express.static('public')); // Public Folder
 app.use(bodyParser.json()); // Support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Support encoded bodies
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 //========
 // Routing
 //========
